17b06a13003477988749bd2fbdcb59a94e6e9fe7
quentinDyma <quentindevial19@gmail.com>
2020-12-08 12:58:28 +0100

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CocktailContainerComponent } from './cocktail-container/cocktail-container.component';
+import { CocktailDetailsComponent } from './cocktail-container/cocktail-details/cocktail-details.component';
+import { CocktailListComponent } from './cocktail-container/cocktail-list/cocktail-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    CocktailListComponent,
+    CocktailDetailsComponent,
+    CocktailContainerComponent,
+  ],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
